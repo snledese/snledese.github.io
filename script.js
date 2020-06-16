7a4a32cf454a8dee5e9beba8ea28bb1cc10fdc59
Roberto Lozano <rlozano@ucdavis.edu>
2020-06-15 21:13:59 -0700

    fixed weird bug and introduced language changing

--- script.js
+++ script.js
@@ -1,170 +1,30 @@
-window.sr = ScrollReveal();
+let choose_language = document.getElementById("choose_language");
+choose_language.addEventListener("click", choose_language_func);
 
-sr.reveal('.navbarName', {
-  duration: 2000,
-  origin:'left',
-  distance:'50px'
-});
+let english = 1;
+let spanish = 0;
+let language = english;
 
-
-/*sr.reveal('.navbar-collapse .collapse .in a', {
-  duration: 2000,
-  origin:'left',
-  distance:'50px'
-});
-
-
-sr.reveal('#nav1 #navbar #home', {
-  delay: 500,
-  duration: 2000,
-  origin:'left',
-  distance:'0px'
-});
-sr.reveal('#nav1 #navbar #about', {
-  delay: 900,
-  duration: 2000,
-  origin:'left',
-  distance:'0px'
-});
-sr.reveal('#nav1 #navbar #portfolio', {
-  delay: 1300,
-  duration: 2000,
-  origin:'left',
-  distance:'0px'
-});
-sr.reveal('#nav1 #navbar #resume', {
-  delay: 1600,
-  duration: 2000,
-  origin:'left',
-  distance:'0px'
-});*/
-
-sr.reveal('.clock', {
-  delay: 1100,
-  duration: 1500,
-  origin:'top',
-  distance:'300px',
-  viewFactor: 0.2
-});
-sr.reveal('.firstSection-left', {
-  duration: 2000,
-  origin:'left',
-  distance:'300px'
-});
-sr.reveal('.firstSection-right', {
-  duration: 2000,
-  origin:'right',
-  distance:'300px'
-});
-sr.reveal('.secondSection-left', {
-  duration: 2000,
-  origin:'bottom',
-  distance:'100px',
-  viewFactor: 0.2
-});
-sr.reveal('.secondSection-right', {
-  duration: 2000,
-  origin:'right',
-  distance:'300px',
-  viewFactor: 0.2
-});
-sr.reveal('.secondSection-right #list1', {
-  delay: 300,
-  duration: 2000,
-  origin:'right',
-  distance:'300px',
-  viewFactor: 0.2
-});
-sr.reveal('.secondSection-right #list2', {
-  delay: 700,
-  duration: 2000,
-  origin:'right',
-  distance:'300px',
-  viewFactor: 0.2
-});
-sr.reveal('.secondSection-right #list3', {
-  delay: 1100,
-  duration: 2000,
-  origin:'right',
-  distance:'300px',
-  viewFactor: 0.2
-});
-sr.reveal('.secondSection-right #list4', {
-  delay: 1500,
-  duration: 2000,
-  origin:'right',
-  distance:'300px',
-  viewFactor: 0.2
-});
-sr.reveal('.secondSection-right #list5', {
-  delay: 1900,
-  duration: 2000,
-  origin:'right',
-  distance:'300px',
-  viewFactor: 0.2
-});
-sr.reveal('.secondSection-right #list6', {
-  delay: 2300,
-  duration: 2000,
-  origin:'right',
-  distance:'300px',
-  viewFactor: 0.2
-});
-sr.reveal('.secondSection-right #list7', {
-  delay: 2700,
-  duration: 2000,
-  origin:'right',
-  distance:'300px',
-  viewFactor: 0.2
-});
-sr.reveal('.secondSection-right #list8', {
-  delay: 3100,
-  duration: 2000,
-  origin:'right',
-  distance:'300px',
-  viewFactor: 0.2
-});
-sr.reveal('.secondSection-right #list9', {
-  delay: 3500,
-  duration: 2000,
-  origin:'right',
-  distance:'300px',
-  viewFactor: 0.2
-});
-sr.reveal('.secondSection-right #list10', {
-  delay: 3900,
-  duration: 2000,
-  origin:'right',
-  distance:'300px',
-  viewFactor: 0.2
-});
-sr.reveal('#socialinfo h2, #socialinfo p', {
-  delay: 700,
-  duration: 1000,
-  origin:'right',
-  distance:'0px',
-  viewFactor: 0.2
-});
-sr.reveal('#socialIcons', {
-  delay: 400,
-  duration: 1000,
-  origin:'right',
-  distance:'0px',
-  viewFactor: 0.2
-});
-
-$(function() {
-  // Smooth Scrolling
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
+function choose_language_func(){
+    console.log(language)
+    if(language == english){
+        console.log("changing to spanish",language)
+        document.getElementById("choose_language").textContent = "English"
+        document.getElementById("mainPic").src = "images/mainPicSpanish.png";
+        document.getElementById("header1").textContent = "Bienvenido a Sembrar!";
+        document.getElementById("paragraph1").textContent = "Nuestro objetivo en Sembar es introducir los conceptos de consentimiento, límites, y autonomía corporal para los niños y sus familias. La esperanza es plantar estas ideas en los primeros años de la infancia los niños estarán equipados con las herramientas necesitan navegar estableciendo límites en las relaciones actuales y futuras."
+        document.getElementById("header2").textContent = "Nuestra Mission";
+        document.getElementById("paragraph2").textContent = "La misión de Sembrar es proporcionar a los padres y cuidadores herramientas para comience la conversación sobre el consentimiento a una edad temprana. Lo sé por mí mismo y mis padres hablar sobre el consentimiento fue difícil y ambos no sabíamos dónde para comenzar. Espero que estas tarjetas brinden a los niños y a sus padres un punto de partida."
+        language = spanish;
     }
-  });
-});
+    else if(language == spanish){
+        console.log("inside span",language)
+        document.getElementById("choose_language").textContent = "Español"
+        document.getElementById("mainPic").src = "images/mainPic.png";
+        document.getElementById("header1").textContent = "Welcome to Sembrar!";
+        document.getElementById("paragraph1").textContent = "Our goal at Sembar is to introduce the concepts of consent, boundaries, and body autonomy to children and their families. The hope is by planting these ideas early in childhood children will be equipped with the tools they need to navigate setting boundaries in current and future relationships."
+        document.getElementById("header2").textContent = "Our mission";
+        document.getElementById("paragraph2").textContent = "The mission of Sembrar is to provide parents and caregivers with tools to begin the conversation about consent at a young age. I know for myself and my parents talking about consent was difficult and we both did not know where to start. I hope that these cards provide children and their parents with a starting point."
+        language = english;
+    }
+}
